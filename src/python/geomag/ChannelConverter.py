ed922239e8685e99ecca0ea1833cff6b781f4ea0
Jeremy Fee <jmfee@usgs.gov>
2014-11-07 14:25:21 -0700

    Interface for conversion between channel formats

---
+++ src/python/geomag/ChannelConverter.py
@@ -0,0 +1,32 @@
+
+
+class ChannelConverter(object):
+  """
+  Convert between different components and coordinate systems.
+  """
+  def __init__(self):
+    pass
+  def getEFromHD(self, h, d):
+    pass
+  def getDFromHE(self, h, e):
+    pass
+  def getHFromXY(self, x, y):
+    pass
+  def getEFromXY(self, x, y):
+    pass
+  def getXFromHE(self, h, e):
+    pass
+  def getYFromHE(self, h, e):
+    pass
+  def getComputedFFromHEZ(self, h, e, z):
+    pass
+  def getComputedFFromXYZ(self, x, y, z):
+    pass
+  def getGFromXYZF(self, x, y, z, f):
+    pass
+  def getGFromHEZF(self, h, e, z, f):
+    pass
+  def getFFromXYZG(self, x, y, z, g):
+    pass
+  def getFFromHEZG(self, h, e, z, g):
+    pass
