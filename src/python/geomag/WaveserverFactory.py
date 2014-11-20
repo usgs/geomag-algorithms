7263f185cd7c01dc6aaf503172ea4843c7cd8652
Jeremy Fee <jmfee@usgs.gov>
2014-11-20 11:23:50 -0700

    Update existing files based on pep8 coding standards

--- src/python/geomag/WaveserverFactory.py
+++ src/python/geomag/WaveserverFactory.py
@@ -1,27 +1,49 @@
-#! /usr/bin/env python
+"""
+Access timeseries from an Earthworm waveserver.
+"""
 
-import sys
 from obspy.core.utcdatetime import UTCDateTime
-from obspy.earthworm import Client
+from obspy import earthworm
 
 
 class WaveserverFactory(object):
-  def __init__(self, host, port):
-    self.client = Client(host, port)
-  # get timeseries for an observatory
-  def getTimeseries(self, observatory, starttime, endtime, location='R0'):
-    c = self.client
-    st = c.getWaveform('NT', observatory, location, 'MVH', starttime, endtime)
-    st += c.getWaveform('NT', observatory, location, 'MVE', starttime, endtime)
-    st += c.getWaveform('NT', observatory, location, 'MVZ', starttime, endtime)
-    return st
+    """
+    Access timeseries from an Earthworm waveserver.
+    """
 
+    def __init__(self, host, port):
+        self.client = earthworm.Client(host, port)
+
+    def getTimeseries(
+            self, observatory, starttime, endtime,
+            channels=('MVH', 'MVE', 'MVZ'), location='R0', network='NT'):
+        """
+        Get timeseries for an observatory.
+        """
+        client = self.client
+        stream = None
+        for channel in channels:
+            data = client.getWaveform(
+                    network, observatory, location, channel,
+                    starttime, endtime)
+            if stream is None:
+                stream = data
+            else:
+                stream += data
+        return stream
+
+
+def main():
+    """
+    main method to test waveserver factory
+    """
+    factory = WaveserverFactory('136.177.50.84', 2060)
+    starttime = UTCDateTime(2014, 6, 12, 5, 0, 0)
+    endtime = UTCDateTime(2014, 6, 12, 5, 30, 0)
+    result = factory.getTimeseries('BOU', starttime, endtime, 'R0')
+    # result += f.getTimeseries('BOU', starttime, endtime, 'R1')
+    print result
+    result.plot()
 
 if __name__ == '__main__':
-  f = WaveserverFactory('136.177.50.84', 2060)
-  starttime = UTCDateTime(2014, 6, 12, 5, 0, 0)
-  endtime = UTCDateTime(2014, 6, 12, 5, 30, 0)
-  result = f.getTimeseries('BOU', starttime, endtime, 'R0')
-  #result += f.getTimeseries('BOU', starttime, endtime, 'R1')
-  print result
-  result.plot()
+    main()
