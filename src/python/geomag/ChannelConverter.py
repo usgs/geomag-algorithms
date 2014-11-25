7fc5e8ae2680ebc11cc8b4dc8faf6ef1afed435d
Hal Simpson <hasimpson@usgs.gov>
2014-11-25 16:56:20 -0700

    added observatory, and magnetic north converters, and tests

--- src/python/geomag/ChannelConverter.py
+++ src/python/geomag/ChannelConverter.py
@@ -2,7 +2,8 @@
 
 import math
 """
-Convert between different components and coordinate systems.
+Convert between different components and coordinate systems used by
+the geomagnetic community.
 """
 
 
@@ -10,7 +11,7 @@ class ChannelConverter(object):
     """
     Convert between different components and coordinate systems.
     We use three coordinate systems.
-    Geo: Based on Geologic North.  X, Y, Z
+    Geo: Based on Geographic North.  X, Y, Z
     Obs: Based on the observatories orientaion. H, E, Z [d]
     Mag: Based on Magnetic North. H, D, Z [E]
     obs_d0: The Decbas can be set during class initialization.
@@ -19,8 +20,22 @@ class ChannelConverter(object):
 
     def __init__(self, obs_d0=0):
         self.obs_d0 = obs_d0
-        pass
 
+    def set_decbas(self, obs_d0):
+        self.obs_d0 = obs_d0
+
+    """
+    get geographic coordinates from....
+    """
+    # general get geo_from_obs save multiple calls.
+    def get_geo_from_obs(self, h, e):
+        mag_h = self.get_mag_h_from_obs(h, e)
+        mag_d = self.get_mag_d_from_obs(h, e)
+        geo_x = self.get_geo_x_from_mag(mag_h, mag_d)
+        geo_y = self.get_geo_y_from_mag(mag_h, mag_d)
+        return (geo_x, geo_y)
+
+    # inividual get obs from calls
     def get_geo_x_from_obs(self, h, e):
         mag_h = self.get_mag_h_from_obs(h, e)
         mag_d = self.get_mag_d_from_obs(h, e)
@@ -37,16 +52,69 @@ class ChannelConverter(object):
     def get_geo_y_from_mag(self, h, d):
         return h * math.sin(d)
 
-    def get_obs_d_from_obs(self, h, e):
-        return math.atan2(e, h)
-
+    """
+    get magnetic north coordinates from....
+    """
     def get_mag_d_from_obs(self, h, e):
         return self.obs_d0 + self.get_obs_d_from_obs(h, e)
+
+    def get_mag_d_from_geo(self, x, y):
+        return math.atan2(y, x)
 
     def get_mag_h_from_obs(self, h, e):
         return math.hypot(h, e)
 
+    def get_mag_h_from_geo(self, x, y):
+        return math.hypot(x, y)
 
+    """
+    get observatory coordinates from....
+    """
+    # general get obs_from_geo save multiple calls.
+    def get_obs_from_geo(self, x, y):
+        mag_h = self.get_mag_h_from_geo(x, y)
+        mag_d = self.get_mag_d_from_geo(x, y)
+        obs_e = self.get_obs_e_from_mag(mag_h, mag_d)
+        obs_h = self.get_obs_h_from_mag(mag_h, mag_d)
+        obs_d = self.get_obs_d_from_mag(mag_d)
+        return (obs_h, obs_e, obs_d)
+
+    # inividual get obs from calls
+    def get_obs_d_from_obs(self, h, e):
+        return math.atan2(e, h)
+
+    def get_obs_d_from_mag(self, d):
+        return d - self.obs_d0
+
+    def get_obs_e_from_geo(self, x, y):
+        mag_h = self.get_mag_h_from_geo(x, y)
+        mag_d = self.get_mag_d_from_geo(x, y)
+        return self.get_obs_e_from_mag(mag_h, mag_d)
+
+    def get_obs_e_from_obs(self, h, d):
+        return h * math.tan(d)
+
+    def get_obs_e_from_mag(self, h, d):
+        obs_d = self.get_obs_d_from_mag(d)
+        return h * math.sin(obs_d)
+
+    def get_obs_h_from_geo(self, x, y):
+        mag_h = self.get_mag_h_from_geo(x, y)
+        mag_d = self.get_mag_d_from_geo(x, y)
+        return self.get_obs_h_from_mag(mag_h, mag_d)
+
+    def get_obs_h_from_mag(self, h, d):
+        obs_d = self.get_obs_d_from_mag(d)
+        return h * math.cos(obs_d)
+
+"""
+short main to test the principal parts work with geomagnetic data.
+"""
 if __name__ == '__main__':
-    channel = ChannelConverter((552.7 * math.pi / 60 / 180))
+    channel = ChannelConverter((552.7 * math.pi / 60.0 / 180.0))
     print "get_geo_x_from_obs", channel.get_geo_x_from_obs(20840.15, -74.16)
+    print "get_geo_y_from_obs", channel.get_geo_y_from_obs(20840.15, -74.16)
+    print "get_obs_e_from_geo", channel.get_obs_e_from_geo(20583.260646,
+            3262.93317535)
+    print "get_obs_h_from_geo", channel.get_obs_h_from_geo(20583.260646,
+            3262.93317535)
--- src/python/geomag/ChannelConverterTest.py
+++ src/python/geomag/ChannelConverterTest.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 import math
-from nose.tools import assert_equals
+from nose.tools import assert_equals, assert_sequence_equal
 from ChannelConverter import ChannelConverter
 
 
@@ -13,29 +13,68 @@ class ChannelConverterTest:
     def test_check_decbase(self):
         assert_equals(self.channel.obs_d0, 0)
 
-    def test_get_geo_x_from_obs(self):
-        # Verify that get_geo_x_from_obs returns correct value.
-        assert_equals(self.channel.get_geo_x_from_obs(2, 2),
-            2.0000000000000004)
+    def test_get_geo_from_obs(self):
+        assert_sequence_equal(self.channel.get_geo_from_obs(1, math.pi / 4),
+                (1, math.pi / 4))
 
     def test_get_geo_x_from_mag(self):
         # Verify that get_geo_x_from_mag returns correct value.
         assert_equals(self.channel.get_geo_x_from_mag(1, math.pi / 4),
             0.7071067811865476)
 
-    def test_get_geo_y_from_obs(self):
-        # Verify that get_geo_y_from_obs returns correct value.
-        assert_equals(self.channel.get_geo_y_from_obs(2, 2),
-            2.0)
+    def test_get_geo_x_from_obs(self):
+        # Verify that get_geo_x_from_obs returns correct value.
+        assert_equals(self.channel.get_geo_x_from_obs(1, math.pi / 4),
+            1)
 
     def test_get_geo_y_from_mag(self):
         # Verify that get_geo_y_from_mag returns correct value.
         assert_equals(self.channel.get_geo_x_from_mag(1, math.pi / 4),
             0.7071067811865476)
 
+    def test_get_geo_y_from_obs(self):
+        # Verify that get_geo_y_from_obs returns correct value.
+        assert_equals(self.channel.get_geo_y_from_obs(1, math.pi / 4),
+            math.pi / 4)
+
+    def test_get_mag_d_from_obs(self):
+        assert_equals(self.channel.get_mag_d_from_obs(2, 2), math.pi / 4)
+
+    def test_get_mag_d_from_geo(self):
+        assert_equals(self.channel.get_mag_d_from_obs(2, 2), math.pi / 4)
+
     def test_get_mag_h_from_obs(self):
         assert_equals(self.channel.get_mag_h_from_obs(3, 4), 5)
 
+    def test_get_mag_h_from_geo(self):
+        assert_equals(self.channel.get_mag_h_from_geo(3, 4), 5)
+
+    def test_get_obs_from_geo(self):
+        assert_sequence_equal(self.channel.get_obs_from_geo(1, math.pi / 4),
+                (1.0, math.pi / 4, 0.6657737500283538))
+
     def test_get_obs_d_from_obs(self):
         assert_equals(self.channel.get_obs_d_from_obs(1, 1),
-                45.0 * math.pi / 180.0)
+                math.pi / 4)
+
+    def test_get_obs_d_from_mag(self):
+        assert_equals(self.channel.get_obs_d_from_mag(1), 1)
+
+    def test_get_obs_e_from_geo(self):
+        assert_equals(self.channel.get_obs_e_from_geo(2, 2), 2)
+
+    def test_get_obs_e_from_obs(self):
+        assert_equals(self.channel.get_obs_e_from_obs(2, math.pi / 4),
+                1.9999999999999998)
+
+    def test_get_obs_e_from_mag(self):
+        assert_equals(self.channel.get_obs_e_from_mag(1, math.pi / 4),
+                0.7071067811865475)
+
+    def test_get_obs_h_from_geo(self):
+        assert_equals(self.channel.get_obs_h_from_geo(2, math.pi / 4),
+                1.9999999999999998)
+
+    def test_get_obs_h_from_mag(self):
+        assert_equals(self.channel.get_obs_h_from_mag(1, math.pi / 4),
+                0.7071067811865476)
