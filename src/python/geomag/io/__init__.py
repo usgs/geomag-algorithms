111b4364566cee85bc5fe02f78e4854eaee86223
Jeremy Fee <jmfee@usgs.gov>
2014-12-02 21:44:19 -0600

    Import timeseries classes into io module

--- src/python/geomag/io/__init__.py
+++ src/python/geomag/io/__init__.py
@@ -1,3 +1,16 @@
 """
 Geomag Timeseries IO Module
 """
+
+from Timeseries import Timeseries
+from TimeseriesException import TimeseriesException
+from TimeseriesFactory import TimeseriesFactory
+from TimeseriesFactoryException import TimeseriesFactoryException
+
+
+__all__ = [
+    'Timeseries',
+    'TimeseriesException',
+    'TimeseriesFactory',
+    'TimeseriesFactoryException'
+]
