8369c70d3d9de96bbce0536296f84adaa32e7882
Jeremy Fee <jmfee@usgs.gov>
2014-12-02 21:45:41 -0600

    Import IAGA2002 factory and parser into geomag.io.iaga2002 package

--- src/python/geomag/io/iaga2002/__init__.py
+++ src/python/geomag/io/iaga2002/__init__.py
@@ -1,6 +1,16 @@
-"""
-IO Module for IAGA 2002 Format
+"""IO Module for IAGA 2002 Format
 
 Based on documentation at:
   http://www.ngdc.noaa.gov/IAGA/vdat/iagaformat.html
 """
+
+from IAGA2002Factory import IAGA2002Factory
+from IAGA2002Parser import IAGA2002Parser
+from MagWebFactory import MagWebFactory
+
+
+__all__ = [
+    'IAGA2002Factory',
+    'IAGA2002Parser',
+    'MagWebFactory'
+]
