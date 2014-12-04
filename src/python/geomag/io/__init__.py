ef35bb804c8c10da06702b35e3479ad63af8242f
Jeremy Fee <jmfee@usgs.gov>
2014-12-04 11:33:50 -0700

    Remove unused import/export from geomag io module init

--- src/python/geomag/io/__init__.py
+++ src/python/geomag/io/__init__.py
@@ -2,15 +2,11 @@
 Geomag Timeseries IO Module
 """
 
-from Timeseries import Timeseries
-from TimeseriesException import TimeseriesException
 from TimeseriesFactory import TimeseriesFactory
 from TimeseriesFactoryException import TimeseriesFactoryException
 
 
 __all__ = [
-    'Timeseries',
-    'TimeseriesException',
     'TimeseriesFactory',
     'TimeseriesFactoryException'
 ]
