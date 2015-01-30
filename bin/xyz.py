d55c343cb481b1dddbc6b59ebe40cde47c134a85
Hal Simpson <hasimpson@usgs.gov>
2015-01-30 15:46:03 -0700

    added comments, changed e_out to d_out, and added type=bool to it's add_argument call

--- bin/xyz.py
+++ bin/xyz.py
@@ -13,6 +13,7 @@ import geomagio.iaga2002 as iaga2002
 from geomagio.iaga2002.IAGA2002Factory import read_url
 import geomagio.StreamConverter as StreamConverter
 
+# static containing the standard output types for iaga2002 files.
 CHANNELS = {
     'geo': ['X', 'Y', 'Z', 'F'],
     'mag': ['H', 'D', 'Z', 'F'],
@@ -20,15 +21,42 @@ CHANNELS = {
     }
 
 def get_out_channels(args):
-    """
+    """Returns the output channels based on the flags.
+
+    Parameteers
+    -----------
+    args: namespace/dictionary
+        outformat: string
+            indicates the desired output format
+        d_out: boolean
+            indicates if the 'observatory' ouput format should include d
+                instead of e
+
+    Returns
+    -------
+    array of strings
+        array of output channels
     """
     format = args.outformat
-    # obs is only HEZF if --e_out flag is set.  Otherwise it's HDZF same as mag
-    if format == 'obs' and args.e_out == False:
+    # obs is only HEZF if --d_out flag is not set.
+    # Otherwise it's HDZF same as mag
+    if format == 'obs' and args.d_out == True:
         format = 'mag'
     return CHANNELS[format]
 
 def convert_stream(args, timeseries):
+    """converts a timeseries stream into a different coordinate system
+
+    Parameters
+    ----------
+    args: namespace/dictionary
+        outformat: string
+            indicates the output coordinate system.
+        informat: string
+            indicates the input coordinate system.
+    out_stream: obspy.core.Stream
+        new stream object containing the converted coordinates.
+    """
     out_stream = None
     if args.outformat == 'geo' and args.informat == 'mag':
         out_stream = StreamConverter.get_geo_from_mag(timeseries)
@@ -46,17 +74,39 @@ def convert_stream(args, timeseries):
         out_stream = StreamConverter.get_obs_from_mag(timeseries)
 
     if args.outformat == 'obs' and args.informat == 'geo':
-        out_stream = StreamConverter.get_obs_from_geo(timeseries)
+        out_stream = StreamConverter.get_obs_from_geo(timeseries, args.d_out)
 
     if args.outformat == 'obs' and args.informat == 'obs':
         out_stream = StreamConverter.get_obs_from_obs(timeseries,
-          args.e_out, True)
+          True, args.d_out)
 
     return out_stream
 
 
 
 def main():
+    """Converts iaga2002 files from one coordinate system to another.
+
+    Inputs
+    ------
+    informat: string
+        The input format/coordinate system of the input file.
+            geo: geographic coordinate system (xyzf)
+            mag: magnetic north coordinate system (hdzf)
+            obs: observatory coordinate system (hezf or hdzf)
+    outformat: string
+        The ouput format/coordinate system of the output file.
+            geo: geographic coordinate system (xyzf)
+            mag: magnetic north coordinate system (hdzf)
+            obs: observatory coordinate system (hezf or hdzf)
+    infile: string
+        the filename of the Iaga2002 file to be read from
+    outfile: string
+        the filename of a new Iaga2002 file to be read to
+    d_out: boolean
+        a flag indicating whether the output for the observatory coordinate
+            system should have the e coordinate instead of d.
+    """
     parser = argparse.ArgumentParser(
         description='Use @ to read commands from a file.',
         fromfile_prefix_chars='@')
@@ -67,8 +117,9 @@ def main():
         default='geo')
     parser.add_argument('--infile', help='iaga2002 input file')
     parser.add_argument('--outfile', help='iaga2002 out file')
-    parser.add_argument('--e_out',
-        help='output E in the IAGA2002 file instead of D',
+    parser.add_argument('--d_out',
+        help='output D in the IAGA2002 file instead of E for obs coordinates',
+        type=bool,
         default=False)
 
     args = parser.parse_args()
@@ -86,6 +137,7 @@ def main():
     timeseries = factory.parse_file(iagaFile)
     out_stream = convert_stream(args, timeseries)
     channels = get_out_channels(args)
+    print channels
     if args.outfile != None:
         fh = open(args.outfile, 'w')
     else:
