143561dda50845c565a2edde8518694e386aab80
Jeremy Fee <jmfee@usgs.gov>
2015-02-11 12:13:27 -0700

    Add setup script for geomagio

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='geomag-algorithms',
+    version='0.0.0',
+    desription='USGS Geomag IO Library',
+    url='https://github.com/usgs/geomag-algorithms',
+    packages=[
+        'geomagio',
+        'geomagio.iaga2002'
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'obspy'
+    ],
+    scripts=[
+        'bin/xyz.py'
+    ]
+)
