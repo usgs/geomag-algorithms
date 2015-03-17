c669d25f7dee0e16f58ba4f677c2af3a58cf245a
Hal Simpson <hasimpson@usgs.gov>
2015-03-16 21:07:19 -0600

    Refactored tests to reflect changes in ObservatoryMetadata class

--- geomagio/edge/ObservatoryMetadata_test.py
+++ geomagio/edge/ObservatoryMetadata_test.py
@@ -7,9 +7,6 @@ import obspy.core
 
 METADATA = {
     'BOU': {
-        'network': 'NT',
-        'station': 'BOU',
-        'channel': 'H',
         'station_name': 'Boulder',
         'agency_name': 'United States Geological Survey (USGS)',
         'geodetic_latitude': -90,
@@ -17,12 +14,10 @@ METADATA = {
         'elevation': -1000,
         'sensor_orientation': 'HDZF',
         'sensor_sampling_rate': '0.01 second',
-        'data_type': 'variation',
-        'data_interval': 'minute',
         'data_interval_type': 'filtered 1-minute (00:15-01:45)',
         'declination_base': 20000,
         'is_intermagnet': False,
-        'condtions_of_use': 'The Conditions of Use for data provided' +
+        'conditions_of_use': 'The Conditions of Use for data provided' +
                 ' through INTERMAGNET and acknowledgement templates can be' +
                 ' found at www.intermagnet.org',
         'filter_comments': 'Vector 1-minute values are computed from' +
@@ -30,9 +25,6 @@ METADATA = {
                 ' centered on the  minute. Scalar 1-minute values are' +
                 ' computed from 1-secondvalues  using the INTERMAGNET' +
                 ' gaussian filter centered on the minute. ',
-        'comments': ' # This data file was constructed by the Golden GIN.' +
-                'Final data will be available on the INTERMAGNET DVD. Go to ' +
-                'www.intermagnet.org for details on obtaining this product. '
     }
 }
 
@@ -49,14 +41,13 @@ def test_set_metadata():
     stats.location = 'R0'
     stats.data_interval = 'second'
     stats.data_type = 'quasi-definitive'
-    observatorymetadata.set_metadata(stats, 'BOU')
-    assert_equals(stats['channel'], 'MVH')
-    assert_equals(stats['data_interval'], 'second')
-    assert_equals(stats['data_type'], 'quasi-definitive')
+    observatorymetadata.set_metadata(stats, 'BOU', 'MVH',
+            'quasi-definitive', 'second')
     assert_equals(stats['declination_base'], 7406)
 
     # Test custom metadata
     stats = obspy.core.Stats()
     observatorymetadata = ObservatoryMetadata(METADATA)
-    observatorymetadata.set_metadata(stats, 'BOU')
+    observatorymetadata.set_metadata(stats, 'BOU', 'MVH',
+            'quasi-definitive', 'second')
     assert_equals(stats['declination_base'], 20000)
