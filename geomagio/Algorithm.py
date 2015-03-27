2f5f7fd72f0180cdd3ddf5fa3ac2fb6299417415
Hal Simpson <hasimpson@usgs.gov>
2015-03-27 17:22:25 -0600

    Added channel handling to base class, since the Controller uses the algorithm to confirm classes into and out of the Algorithm

--- geomagio/Algorithm.py
+++ geomagio/Algorithm.py
@@ -4,6 +4,10 @@ class Algorithm(object):
     """An algorithm processes a stream of timeseries to produce new timeseries.
     """
 
+    def __init__(self, channels=None):
+        self._channels = channels
+        pass
+
     def process(self, stream):
         """Process a chunk of data.
 
@@ -18,3 +22,9 @@ class Algorithm(object):
             resulting data
         """
         return stream.copy()
+
+    def get_input_channels(self):
+        return self._channels
+
+    def get_output_channels(self):
+        return self._channels
