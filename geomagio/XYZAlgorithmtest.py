7bce2122a538da301f49a35a62afb2eb46783ca1
Hal Simpson <hasimpson@usgs.gov>
2015-03-31 10:04:19 -0600

    Added tests for XYZAlgorithm

---
+++ geomagio/XYZAlgorithmtest.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+from obspy.core.stream import Stream
+from nose.tools import assert_equals
+from nose.tools import assert_true
+from XYZAlgorithm import XYZAlgorithm
+from StreamConverter_test import __create_trace
+
+
+def test_xyzalgorithm_process():
+    """geomag.XYZAlgorithmtest.test_algorithm_process()
+
+    confirms that a converted stream contains the correct outputchannels.
+    """
+    algorithm = XYZAlgorithm('obs', 'geo')
+    timeseries = Stream()
+    timeseries += __create_trace('H', [1, 1])
+    timeseries += __create_trace('E', [1, 1])
+    timeseries += __create_trace('Z', [1, 1])
+    timeseries += __create_trace('F', [1, 1])
+    outputstream = algorithm.process(timeseries)
+    assert_true(algorithm.check_stream(outputstream,
+            algorithm.get_output_channels()))
+
+
+def test_xyzalgorithm_channels():
+    """geomag.XYZAlgorithmtest.test_algorithm_process()
+
+    confirms that the input/output channels are correct for the given
+    informat/outformat during instantiation.
+    """
+    algorithm = XYZAlgorithm('obs', 'geo')
+    inchannels = ['H', 'E', 'Z', 'F']
+    outchannels = ['X', 'Y', 'Z', 'F']
+    assert_equals(algorithm.get_input_channels(), inchannels)
+    assert_equals(algorithm.get_output_channels(), outchannels)
