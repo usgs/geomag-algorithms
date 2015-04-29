400264fb966cbbc6c29d6879d1ac51d8366b1d12
Hal Simpson <hasimpson@usgs.gov>
2015-04-29 10:34:04 -0600

    removed unneeded WaveServerFactory call

--- geomagio/__init__.py
+++ geomagio/__init__.py
@@ -6,7 +6,6 @@ import ChannelConverter
 import StreamConverter
 from TimeseriesFactory import TimeseriesFactory
 from TimeseriesFactoryException import TimeseriesFactoryException
-from WaveserverFactory import WaveserverFactory
 
 __all__ = [
     'Algorithm',
