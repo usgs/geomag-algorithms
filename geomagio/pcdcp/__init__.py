76add53256e9748aeeaafd5a935898a766b4775c
Eddie McWhirter <emcwhirter@usgs.gov>
2015-06-20 15:25:52 -0600

    Add __init__ file to pcdcp directory.

---
+++ geomagio/pcdcp/__init__.py
@@ -0,0 +1,15 @@
+"""IO Module for PCDCP Format
+"""
+
+from PCDCPFactory import PCDCPFactory
+from StreamPCDCPFactory import StreamPCDCPFactory
+from PCDCPParser import PCDCPParser
+from PCDCPWriter import PCDCPWriter
+
+
+__all__ = [
+    'PCDCPFactory',
+    'StreamPCDCPFactory',
+    'PCDCPParser',
+    'PCDCPWriter'
+]
