0134a084d7f9a998b3ea89e0f3c898cbd157b851
Hal Simpson <hasimpson@usgs.gov>
2015-07-07 13:03:21 -0600

    pulled out debug code

--- bin/geomag.py
+++ bin/geomag.py
@@ -193,9 +193,9 @@ def parse_args():
             default='variation',
             choices=['variation', 'quasi-definitive', 'definitive'])
     parser.add_argument('--locationcode',
-            choices=['R0', 'R1', 'RM', 'Q0', 'D0', 'C0', 'Z1'])
+            choices=['R0', 'R1', 'RM', 'Q0', 'D0', 'C0'])
     parser.add_argument('--outlocationcode',
-            choices=['R0', 'R1', 'RM', 'Q0', 'D0', 'C0', 'Z1'])
+            choices=['R0', 'R1', 'RM', 'Q0', 'D0', 'C0'])
     parser.add_argument('--interval',
             default='minute',
             choices=['minute', 'second'])
