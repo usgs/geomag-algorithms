e2e2a2d6419fbd495c7ad71073d6c9ac947fbf1b
Jeremy Fee <jmfee@usgs.gov>
2015-07-08 14:36:45 -0600

    Add comment for ObjectView class

--- geomagio/Util.py
+++ geomagio/Util.py
@@ -1,6 +1,19 @@
+
+
 class ObjectView(object):
+    """
+    Wrap a dictionary so its properties can be accessed as an object.
+
+    Parameters
+    ----------
+    d : dictionary
+        The dictionary to wrap.
+    """
     def __init__(self, d):
         self.__dict__ = d
 
     def __str__(self):
+        """
+        Override string representation to output wrapped dictionary.
+        """
         return str(self.__dict__)
