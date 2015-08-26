8e3a9e0f7d74bf79e39ed3b458d1a0728cacabca
Hal Simpson <hasimpson@usgs.gov>
2015-08-26 10:59:52 -0600

    moved ObservatoryMetadata into geomagio.

--- geomagio/edge/__init__.py
+++ geomagio/edge/__init__.py
@@ -2,12 +2,9 @@
 """
 
 from EdgeFactory import EdgeFactory
-from ObservatoryMetadata import ObservatoryMetadata
 from RawInputClient import RawInputClient
 
 __all__ = [
     'EdgeFactory',
-
-    'ObservatoryMetadata',
     'RawInputClient'
 ]
