56c5bc3da92531a79d19cd08a5bdb5128381eb03
Jeremy Fee <jmfee@usgs.gov>
2015-10-08 14:01:26 -0600

    Fix setup.py syntax.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='geomag-algorithms',
     version='0.0.0',
-    desription='USGS Geomag IO Library',
+    description='USGS Geomag IO Library',
     url='https://github.com/usgs/geomag-algorithms',
     packages=[
         'geomagio',
         'geomagio.iaga2002',
-        'geomagio.imvf283',
+        'geomagio.imfv283',
         'geomagio.edge',
         'geomagio.pcdcp'
     ],
