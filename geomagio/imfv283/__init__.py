e631dd050701df9089b225aeb3427011dcb7f3f2
Hal Simpson <hasimpson@usgs.gov>
2015-11-04 15:35:01 -0700

    Added GOESIMFV283Factory

--- geomagio/imfv283/__init__.py
+++ geomagio/imfv283/__init__.py
@@ -4,12 +4,14 @@ Based on documentation at:
   http://http://www.intermagnet.org/data-donnee/formats/imfv283e-eng.php
 """
 
+from GOESIMFV283Factory import GOESIMFV283Factory
 from IMFV283Factory import IMFV283Factory
 from StreamIMFV283Factory import StreamIMFV283Factory
 from IMFV283Parser import IMFV283Parser
 
 
 __all__ = [
+    'GOESIMFV283Factory'
     'IMFV283Factory',
     'StreamIMFV283Factory',
     'IMFV283Parser'
