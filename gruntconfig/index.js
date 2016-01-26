c1aba113d27943d107d4db76dd88d83fc679a397
Jeremy Fee <jmfee@usgs.gov>
2016-01-26 13:28:16 -0700

    Add grunt clean task to clean up *.pyc files

--- gruntconfig/index.js
+++ gruntconfig/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 module.exports = {
+  clean: require('./clean'),
   flake8: require('./flake8'),
   jshint: require('./jshint'),
   nose: require('./nose'),
   watch: require('./watch'),
   // task node module names
   tasks: [
+    'grunt-contrib-clean',
     'grunt-contrib-jshint',
     'grunt-contrib-watch',
     'grunt-flake8',
