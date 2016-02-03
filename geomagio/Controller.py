838c0c0dca2a722bcf04e81f80e6e5c0a19e885d
Eddie McWhirter <emcwhirter@usgs.gov>
2016-02-03 14:04:23 -0700

    Don't trim if --no-trim is set.

--- geomagio/Controller.py
+++ geomagio/Controller.py
@@ -159,7 +159,9 @@ class Controller(object):
                     timeseries=timeseries,
                     renames=options.rename_input_channel)
         processed = algorithm.process(timeseries)
-        processed.trim(starttime=options.starttime, endtime=options.endtime)
+        # trim if --no-trim is not set
+        if not options.trim:
+            processed.trim(starttime=options.starttime, endtime=options.endtime)
         if options.rename_output_channel:
             processed = self._rename_channels(
                     timeseries=processed,
