8fa62bac441405250a7900d7a9e2df980f12e8c2
abe <aclaycomb@usgs.gov>
2016-04-06 16:32:16 -0600

    added boulder matrix json file for test, and a trivial test

--- test/algorithm_test/AdjustedAlgorithm_test.py
+++ test/algorithm_test/AdjustedAlgorithm_test.py
@@ -4,6 +4,9 @@ from nose.tools import assert_equals
 from nose.tools import assert_almost_equals
 
 def test_construct():
+    """
+    path to
+    """
     matrix = None
     pier_correction = None
     a = adj.AdjustedAlgorithm(matrix,pier_correction,'etc/adjusted/adjbou_state_.json')
@@ -11,3 +14,9 @@ def test_construct():
     assert_almost_equals(a.matrix[0,0], 9.83427577e-01, 6)
 
     assert_equals(a.pier_correction,-22)
+
+def test_trivial():
+    """
+    """
+
+    assert_equals(3,3)
