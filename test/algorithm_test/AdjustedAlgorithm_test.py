25025f3ccc6cff77b5264bc582862b66d82cb241
abe <aclaycomb@usgs.gov>
2016-04-06 16:20:56 -0600

    Phase 1 of addressing Issue #47.  Needs more tests, to be added later

---
+++ test/algorithm_test/AdjustedAlgorithm_test.py
@@ -0,0 +1,13 @@
+from geomagio.algorithm import AdjustedAlgorithm as adj
+import numpy as np
+from nose.tools import assert_equals
+from nose.tools import assert_almost_equals
+
+def test_construct():
+    matrix = None
+    pier_correction = None
+    a = adj.AdjustedAlgorithm(matrix,pier_correction,'etc/adjusted/adjbou_state_.json')
+
+    assert_almost_equals(a.matrix[0,0], 9.83427577e-01, 6)
+
+    assert_equals(a.pier_correction,-22)
