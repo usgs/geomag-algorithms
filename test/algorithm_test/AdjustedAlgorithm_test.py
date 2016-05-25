c8d07dfbd3e9f552a9af8c72b01ac50ae302834b
abe <aclaycomb@usgs.gov>
2016-05-24 18:24:15 -0600

    changed test_process to run from files vs network

--- test/algorithm_test/AdjustedAlgorithm_test.py
+++ test/algorithm_test/AdjustedAlgorithm_test.py
@@ -1,8 +1,7 @@
 from geomagio.algorithm import AdjustedAlgorithm as adj
-from geomagio.edge import EdgeFactory as edge
+import geomagio.iaga2002 as i2
 from nose.tools import assert_equals
 from nose.tools import assert_almost_equals
-from obspy.core import UTCDateTime
 
 
 def test_construct():
@@ -20,19 +19,33 @@ def test_construct():
 
 def test_process():
     """
+    Check adjusted data processing versus files generated from
+    original script
     """
     matrix = None
     pier_correction = None
     a = adj(matrix, pier_correction, 'etc/adjusted/adjbou_state_.json')
 
-    factory = edge(host='137.227.224.97', port=2060)
-    hezf = factory.get_timeseries(observatory='BOU',
-        interval='minute',
-        type='variation',
-        channels=('H', 'E', 'Z', 'F'),
-        starttime=UTCDateTime('2016-01-01T00:00:00Z'),
-        endtime=UTCDateTime('2016-01-01T00:05:00Z'))
+    hezf_iaga2002_file = open('etc/adjusted/BOU201601vmin.min')
+    hezf_iaga2002_string = hezf_iaga2002_file.read()
+    xyzf_iaga2002_file = open('etc/adjusted/BOU201601adj.min')
+    xyzf_iaga2002_string = xyzf_iaga2002_file.read()
+    factory = i2.IAGA2002Factory()
+    hezf = factory.parse_string(hezf_iaga2002_string)
+    xyzf = factory.parse_string(xyzf_iaga2002_string)
 
     adj_bou = a.process(hezf)
+    x = xyzf.select(channel='X')[0]
+    y = xyzf.select(channel='Y')[0]
+    z = xyzf.select(channel='Z')[0]
+    f = xyzf.select(channel='F')[0]
+    x_adj = adj_bou.select(channel='X')[0]
+    y_adj = adj_bou.select(channel='Y')[0]
+    z_adj = adj_bou.select(channel='Z')[0]
+    f_adj = adj_bou.select(channel='F')[0]
 
-    assert_equals(hezf[0].data[0] == adj_bou[0].data[0], False)
+    for r in range(hezf[0].data.size):
+        assert_almost_equals(x.data[r], x_adj.data[r], 1)
+        assert_almost_equals(y.data[r], y_adj.data[r], 1)
+        assert_almost_equals(z.data[r], z_adj.data[r], 1)
+        assert_almost_equals(f.data[r], f_adj.data[r], 1)
