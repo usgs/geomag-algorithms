5f458c7cb556eb530ba09c1c46a9fbc895371f6e
Jeremy Fee <jmfee@usgs.gov>
2017-02-02 09:22:21 -0700

    Update Util.read_url to use BytesIO

--- geomagio/Util.py
+++ geomagio/Util.py
@@ -2,7 +2,7 @@ import pycurl
 import numpy
 import os
 from obspy.core import Stats, Trace
-from io import StringIO
+from io import BytesIO
 
 
 class ObjectView(object):
@@ -161,7 +161,7 @@ def read_url(url, connect_timeout=15, max_redirects=5, timeout=300):
     except Exception:
         pass
     content = None
-    out = StringIO()
+    out = BytesIO()
     curl = pycurl.Curl()
     try:
         curl.setopt(pycurl.FOLLOWLOCATION, 1)
@@ -173,6 +173,7 @@ def read_url(url, connect_timeout=15, max_redirects=5, timeout=300):
         curl.setopt(pycurl.WRITEFUNCTION, out.write)
         curl.perform()
         content = out.getvalue()
+        content = content.decode('utf-8')
     except pycurl.error as e:
         raise IOError(e.args)
     finally:
