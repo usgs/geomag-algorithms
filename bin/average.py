ba0b6de341939496e648449c6ab11901aff13bc3
Rigdon <arigdon@igskcicgltgm031.gs.doi.net>
2017-07-26 07:53:39 -0600

    Created a new script to call average functin to make the interface friendlier

---
+++ bin/average.py
@@ -0,0 +1,19 @@
+#! /Users/arigdon/miniconda3/envs/avgenv/bin/env/python
+
+from os import path
+import sys
+# ensure geomag is on the path before importing
+try:
+    import geomagio  # noqa (tells linter to ignore this line.)
+except:
+    script_dir = path.dirname(path.abspath(__file__))
+    sys.path.append(path.normpath(path.join(script_dir, '..')))
+
+
+from geomagio.Controller import main, parse_args
+
+
+if __name__ == '__main__':
+    args = parse_args(sys.argv[1:])
+    args.algorithm = 'average'
+    main(args)
