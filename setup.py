36da051f087de550fa6e5e0308649b9cdc4f2ffa
erigler-usgs <erigler@usgs.gov>
2018-08-23 09:38:39 -0600

    v0.7.1
    Update version to 0.7.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='geomag-algorithms',
-    version='0.7.0',
+    version='0.7.1',
     description='USGS Geomag IO Library',
     url='https://github.com/usgs/geomag-algorithms',
     packages=[
