fa638251d7d080fb9a0ba1b4fefb1a58c24d039e
E. Joshua Rigler <erigler@usgs.gov>
2018-10-01 13:16:05 -0600

    Small fix to SqDistAlgorithm initiization
    Use NumPy's `isnan(var)` function instead of `var is np.nan`, since
    the latter doesn't work if var is a 64bit float NaN.

--- geomagio/algorithm/SqDistAlgorithm.py
+++ geomagio/algorithm/SqDistAlgorithm.py
@@ -332,7 +332,7 @@ class SqDistAlgorithm(Algorithm):
         # set some default values
         if l0 is None:
             l = np.nanmean(yobs[0:int(m)])
-            if l is np.nan:
+            if np.isnan(l):
                 l = 0.
         else:
             l = l0
