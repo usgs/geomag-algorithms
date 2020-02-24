31ec1c0f1a0eb83af6f0617acb53c7d7e41169be
Travis Rivers <travrivers88@gmail.com>
2020-02-24 16:42:10 -0700

    reorganize code, add more error handling

--- geomagio/webservice/data.py
+++ geomagio/webservice/data.py
@@ -1,13 +1,14 @@
-from datetime import datetime
-import os
-from flask import Blueprint, Flask, jsonify, render_template, request, Response
-from obspy import UTCDateTime
 from collections import OrderedDict
+from datetime import datetime
+from flask import Blueprint, Flask, jsonify, render_template, request, Response
 from json import dumps
+from obspy import UTCDateTime
+import os
 
 from geomagio.edge import EdgeFactory
 from geomagio.iaga2002 import IAGA2002Writer
 from geomagio.imfjson import IMFJSONWriter
+
 
 ERROR_CODE_MESSAGES = {
     204: "No Data",
@@ -19,21 +20,32 @@ ERROR_CODE_MESSAGES = {
     503: "Service Unavailable"
 }
 VALID_DATA_TYPES = ["variation", "adjusted", "quasi-definitive", "definitive"]
+VALID_OBSERVATORIES = ["BRT", "BRW", "DED", "DHT", "CMO", "CMT", "SIT", "SHU", "NEW",
+"BDT", "BOU", "TST", "USGS", "FDT", "FRD", "FRN", "TUC", "BSL", "HON", "SJG",
+"GUA", "SJT"]
 VALID_OUTPUT_FORMATS = ["iaga2002", "json"]
 VALID_SAMPLING_PERIODS = ["1", "60"]
 
+
 blueprint = Blueprint("data", __name__)
-factory = EdgeFactory(
+edge_factory = EdgeFactory(
     host=os.getenv('host', 'cwbpub.cr.usgs.gov'),
     port=os.getenv('port', 2060),
     write_port=os.getenv('write_port', 7981)
    )
 
+
 def init_app(app: Flask):
     global blueprint
-    global factory
+    global edge_factory
 
     app.register_blueprint(blueprint)
+
+
+class WebServiceException(Exception):
+    """Base class for exceptions thrown by web services."""
+    pass
+
 
 class WebServiceQuery(object):
     """Query parameters for a web service request.
@@ -65,7 +77,7 @@ class WebServiceQuery(object):
         elements=("X", "Y", "Z", "F"),
         sampling_period=60,
         data_type="variation",
-        output_format="iaga2002",
+        output_format="iaga2002"
     ):
         self.observatory_id = observatory_id
         self.starttime = starttime
@@ -86,52 +98,56 @@ class WebServiceQuery(object):
             raise WebServiceException(
                 "No more than four elements allowed for iaga2002 format."
             )
+        if self.observatory_id not in VALID_OBSERVATORIES:
+            raise WebServiceException(
+                'Bad observatory ID "%s".'
+                " Valid values are: %s" % (self.observatory_id, ', '.join(VALID_OBSERVATORIES) + '.')
+            )
         if self.starttime > self.endtime:
             raise WebServiceException("Starttime must be before endtime.")
         if self.data_type not in VALID_DATA_TYPES:
             raise WebServiceException(
                 'Bad type value "%s".'
-                " Valid values are: %s" % (self.data_type, VALID_DATA_TYPES)
+                " Valid values are: %s" % (self.data_type, ', '.join(VALID_DATA_TYPES) + '.')
             )
         if self.sampling_period not in VALID_SAMPLING_PERIODS:
             raise WebServiceException(
                 'Bad sampling_period value "%s".'
-                " Valid values are: %s" % (self.sampling_period, VALID_SAMPLING_PERIODS)
+                " Valid values are: %s" % (self.sampling_period, ', '.join(VALID_SAMPLING_PERIODS) + '.')
             )
         if self.output_format not in VALID_OUTPUT_FORMATS:
             raise WebServiceException(
                 'Bad format value "%s".'
-                " Valid values are: %s" % (self.output_format, VALID_OUTPUT_FORMATS)
+                " Valid values are: %s" % (self.output_format, ', '.join(VALID_OUTPUT_FORMATS) + '.')
             )
+
 
 @blueprint.route("/data", methods=["GET"])
 def get_data():
     query_params = request.args
 
-    url = request.url
-
     if not query_params:
-        return render_template("usage.html")
-
-    parsed_query = parse_query(query_params)
+        return render_template("data/usage.html")
 
     try:
+        parsed_query = parse_query(query_params)
         parsed_query._verify_parameters()
     except Exception as e:
         message = str(e)
-        error_body = error(400, message, parsed_query, url)
+        error_body = error(400, message, parsed_query, request)
         return error_body
 
     try:
         timeseries = get_timeseries(parsed_query)
+        return format_timeseries(timeseries, parsed_query)
     except Exception as e:
         message = str(e)
-        error_body = error(500, message, parsed_query, url)
+        error_body = error(500, message, parsed_query, request)
         return error_body
 
-    return format_timeseries(timeseries, parsed_query)
 
 def error(code, message, query, url):
+    """Assign error_body value based on error format."""
     error_body = http_error(code, message, query, url)
     status = str(code) + ' ' + ERROR_CODE_MESSAGES[code]
 
@@ -139,15 +155,127 @@ def error(code, message, query, url):
 
     return error_body
 
-def http_error(code, message, query, url):
+
+def format_timeseries(timeseries, query):
+    """Formats timeseries into JSON or IAGA data
+
+    Parameters
+    ----------
+    obspy.core.Stream
+        timeseries object with requested data
+
+    WebServiceQuery
+        parsed query object
+
+    Returns
+    -------
+    unicode
+        IAGA2002 or JSON formatted string.
+    """
+    if query.output_format == "json":
+        json_output = IMFJSONWriter.format(timeseries, query.elements)
+        json_output = Response(json_output, mimetype="application/json")
+
+        return json_output
+
+    else:
+        iaga_output = IAGA2002Writer.format(timeseries, query.elements)
+        iaga_output = Response(iaga_output, mimetype="text/plain")
+
+        return iaga_output
+
+
+def get_interval(query):
+    """
+    Parameters
+    ----------
+     WebServiceQuery
+        parsed query object
+
+    Returns
+    -------
+    WebServiceQuery
+        with sampling period converted to interval
+    """
+    if query.sampling_period == "1":
+        query.sampling_period = "second"
+
+    if query.sampling_period == "60":
+        query.sampling_period = "minute"
+
+    return query
+
+
+def get_timeseries(query):
+    """
+    Parameters
+    ----------
+     WebServiceQuery
+        parsed query object
+
+    Returns
+    -------
+    obspy.core.Stream
+        timeseries object with requested data
+    """
+    get_interval(query)
+
+    timeseries = edge_factory.get_timeseries(
+        query.starttime,
+        query.endtime,
+        query.observatory_id,
+        query.elements,
+        query.data_type,
+        query.sampling_period)
+
+    return timeseries
+
+
+def http_error(code, message, query, request):
+    """Format http error message.
+
+    Returns
+    -------
+    http_error_body : str
+        body of http error message.
+    """
     if query.output_format == 'json':
-        http_error_body = json_error(code, message, url)
+        http_error_body = json_error(code, message, request.url)
         return http_error_body
     else:
-        http_error_body = iaga2002_error(code, message, url)
+        http_error_body = iaga2002_error(code, message, request.query_string)
         return http_error_body
 
+
+def iaga2002_error(code, message, request_args):
+    """Format iaga2002 error message.
+
+    Returns
+    -------
+    error_body : str
+        body of iaga2002 error message.
+    """
+
+    status_message = ERROR_CODE_MESSAGES[code]
+    error_body = 'Error ' + str(code) + ': ' \
+    + status_message + '\n\n' + message + '\n\n'\
+    + 'Usage details are available from '\
+    + 'http://geomag.usgs.gov/ws/edge/ \n\n'\
+    + 'Request:\n' + 'ws/edge/?' + str(request_args)[2:-1] + '\n\n' + 'Request Submitted:\n'\
+    + datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ") + '\n'
+
+    error_body = Response(error_body, mimetype="text/plain")
+    return error_body
+
+
 def json_error(code, message, url):
+    """Format json error message.
+
+    Returns
+    -------
+    error_body : str
+        body of json error message.
+    """
     error_dict = OrderedDict()
     error_dict['type'] = "Error"
     error_dict['metadata'] = OrderedDict()
@@ -163,17 +291,6 @@ def json_error(code, message, url):
 
     return error_body
 
-def iaga2002_error(code, message, url):
-    status_message = ERROR_CODE_MESSAGES[code]
-    error_body = 'Error ' + str(code) + ': ' \
-    + status_message + '\n\n' + message + '\n\n'\
-    + 'Usage details are available from '\
-    + 'http://geomag.usgs.gov/ws/edge/ \n\n'\
-    + 'Request:\n' + url + '\n\n' + 'Request Submitted:\n'\
-    + datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ") + '\n'
-
-    error_body = Response(error_body, mimetype="text/plain")
-    return error_body
 
 def parse_query(query):
     """Parse request arguments into a set of parameters
@@ -232,63 +349,3 @@ def parse_query(query):
     params.endtime = end_time
 
     return params
-
-def get_timeseries(query):
-    """
-    Parameters
-    ----------
-     WebServiceQuery
-        parsed query object
-
-    Returns
-    -------
-    obspy.core.Stream
-        timeseries object with requested data
-    """
-    if query.sampling_period == "1":
-        query.sampling_period = "second"
-
-    if query.sampling_period == "60":
-        query.sampling_period = "minute"
-
-    timeseries = factory.get_timeseries(
-        query.starttime,
-        query.endtime,
-        query.observatory_id,
-        query.elements,
-        query.data_type,
-        query.sampling_period)
-
-    return timeseries
-
-def format_timeseries(timeseries, query):
-    """Formats timeseries into JSON or IAGA data
-
-    Parameters
-    ----------
-    obspy.core.Stream
-        timeseries object with requested data
-
-    WebServiceQuery
-        parsed query object
-
-    Returns
-    -------
-    unicode
-        IAGA2002 or JSON formatted string.
-    """
-    if query.output_format == "json":
-        json_output = IMFJSONWriter.format(timeseries, query.elements)
-        json_output = Response(json_output, mimetype="application/json")
-
-        return json_output
-
-    else:
-        iaga_output = IAGA2002Writer.format(timeseries, query.elements)
-        iaga_output = Response(iaga_output, mimetype="text/plain")
-
-        return iaga_output
-
-class WebServiceException(Exception):
-    """Base class for exceptions thrown by web services."""
-    pass
