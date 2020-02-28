e23915c17563d725f95f1e90899da991a33c6001
Jeremy Fee <jmfee@usgs.gov>
2020-02-27 19:07:50 -0700

    Update measurement to be per residual, access for multiple measurements by type, and add more comments

---
+++ geomagio/residual/method.py
@@ -0,0 +1,4 @@
+def calculate_absolutes(reading: Reading) -> List[Absolute]:
+    """Use reading measurements and metadata to calculate absolutes.
+    """
+    raise NotImplementedError("TODO: implement this")
