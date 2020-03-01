de90865b19f0bf1394fcc6b4517d1a3f7d2cdde8
Jeremy Fee <jmfee@usgs.gov>
2020-03-01 12:19:34 -0700

    Add SpreadsheetAbsoluteFactory to read residual spreadsheet format

---
+++ geomagio/residual/measurement.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from obspy.core import UTCDateTime
+from pydantic import BaseModel
+
+from .. import pydantic_utcdatetime
+from .MeasurementType import MeasurementType
+
+
+class Measurement(BaseModel):
+    """One angle and time measurement with optional residual.
+
+    Attributes
+    ----------
+    measurement_type: type of measurement.
+    angle: measured angle, decimal degrees.
+    residual: residual at time of measurement.
+    time: when measurement was taken.
+    """
+
+    measurement_type: MeasurementType
+    angle: float = 0
+    residual: float = 0
+    time: Optional[UTCDateTime] = None
