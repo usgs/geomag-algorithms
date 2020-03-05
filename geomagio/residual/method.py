681310bdb45ce06e8e06345e610ab9aebd341684
Jeremy Fee <jmfee@usgs.gov>
2020-03-05 12:14:02 -0700

    Split files (renaming measurement for case change)

--- geomagio/residual/method.py
+++
@@ -1,4 +0,0 @@
-def calculate_absolutes(reading: Reading) -> List[Absolute]:
-    """Use reading measurements and metadata to calculate absolutes.
-    """
-    raise NotImplementedError("TODO: implement this")
