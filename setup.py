688a398d67ab489ddcf78395bac7a635e703cc30
Jeremy Fee <jmfee@usgs.gov>
2020-03-09 10:13:47 -0600

    Re-add numpy and scipy to install_requires

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     url="https://github.com/usgs/geomag-algorithms",
     packages=setuptools.find_packages(exclude=["test*"]),
     python_requires=">=3.6, <4",
-    install_requires=["obspy"],
+    install_requires=["numpy", "scipy", "obspy"],
     extras_require={
         "url": ["pycurl"],
         "webservice": [
