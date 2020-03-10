4e069b61b820d3abce2b582723f57b7d1e0b8402
Jeremy Fee <jmfee@usgs.gov>
2020-03-10 16:22:59 -0600

    Use setuptools-pipfile so dependencies managed in pipfile

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
 import setuptools
+import setuptools.ssl_support
+
+# configure ssl certifiate bundle from environment, if set
+ssl_cert_file = os.environ.get("SSL_CERT_FILE") or os.environ.get("PIP_CERT")
+if ssl_cert_file:
+    setuptools.ssl_support.cert_paths = [ssl_cert_file]
 
 setuptools.setup(
     name="geomag-algorithms",
@@ -6,23 +13,15 @@ setuptools.setup(
     description="USGS Geomag Algorithms Library",
     url="https://github.com/usgs/geomag-algorithms",
     packages=setuptools.find_packages(exclude=["test*"]),
-    python_requires=">=3.6, <4",
-    install_requires=["numpy", "scipy", "obspy"],
-    extras_require={
-        "url": ["pycurl"],
-        "webservice": [
-            "authlib",
-            "flask",
-            "flask-login",
-            "flask-migrate",
-            "flask-session",
-            "flask-sqlalchemy",
-            "psycopg2-binary",
-        ],
-    },
-    scripts=["bin/geomag.py", "bin/geomag_webservice.py", "bin/make_cal.py"],
     project_urls={
         "Bug Reports": "https://github.com/usgs/geomag-algorithms/issues",
         "Source": "https://github.com/usgs/geomag-algorithms",
     },
+    python_requires=">=3.6, <4",
+    scripts=["bin/geomag.py", "bin/geomag_webservice.py", "bin/make_cal.py"],
+    setup_requires=[
+        "setuptools==45.3.0",  # need this until obspy 1.2.1
+        "setuptools-pipfile",
+    ],
+    use_pipfile=True,
 )
