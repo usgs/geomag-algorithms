cfa881c82f18fad704d1cec777a0b9fec526c7bd
pcain-usgs <pcain@usgs.gov>
2020-04-13 16:30:00 -0600

    Delete additional test scripts

--- verify_residuals.py
+++
@@ -1,9 +0,0 @@
-from geomagio.residual import SpreadsheetAbsolutesFactory
-
-saf = SpreadsheetAbsolutesFactory()
-
-reading = saf.parse_spreadsheet(
-    "/Users/pcain/Desktop/Residual Data/DED-20140952243.xlsm"
-)
-
-reading.update_absolutes()
