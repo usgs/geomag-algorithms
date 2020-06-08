9ed0dde84f320384b79aeb3e3b76088eddcebaee
pcain-usgs <pcain@usgs.gov>
2020-06-08 10:43:59 -0600

    Create pcdcp test

--- test/prepfiles_test.py
+++ test/prepfiles_test.py
@@ -20,8 +20,30 @@ def test_prepfiles_raw():
     result_f_data = pcdcp.data["F"]
     result_times = pcdcp.times
 
-    assert_almost_equal(result_h_data, h_data, decimal=3)
-    assert_almost_equal(result_e_data, e_data, decimal=3)
-    assert_almost_equal(result_z_data, z_data, decimal=3)
-    assert_almost_equal(result_f_data, f_data, decimal=3)
-    assert_almost_equal(result_times, times, decimal=0)
+    assert_almost_equal(result_h_data[0:-1], h_data, decimal=3)
+    assert_almost_equal(result_e_data[0:-1], e_data, decimal=3)
+    assert_almost_equal(result_z_data[0:-1], z_data, decimal=3)
+    assert_almost_equal(result_f_data[0:-1], f_data, decimal=3)
+
+
+def test_prepfiles_raw():
+    # gather raw example file from magproc
+    pcdcp = PCDCPParser()
+    pcdcp.parse(open("etc/prepfiles/CMO2020061.min").read())
+    h_data = pcdcp.data["H"]
+    e_data = pcdcp.data["E"]
+    z_data = pcdcp.data["Z"]
+    f_data = pcdcp.data["F"]
+    times = pcdcp.times
+
+    pcdcp.parse(open("etc/prepfiles/CMO202061_cmd.min").read())
+    result_h_data = pcdcp.data["H"]
+    result_e_data = pcdcp.data["E"]
+    result_z_data = pcdcp.data["Z"]
+    result_f_data = pcdcp.data["F"]
+    result_times = pcdcp.times
+
+    assert_almost_equal(result_h_data[0:-1], h_data, decimal=2)
+    assert_almost_equal(result_e_data[0:-1], e_data, decimal=2)
+    assert_almost_equal(result_z_data[0:-1], z_data, decimal=2)
+    assert_almost_equal(result_f_data[0:-1], f_data, decimal=2)
