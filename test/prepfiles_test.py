4e50b3e10fc6b4d103a93738df263e471942388c
pcain-usgs <pcain@usgs.gov>
2020-06-08 10:50:16 -0600

    Remove times from test

--- test/prepfiles_test.py
+++ test/prepfiles_test.py
@@ -11,14 +11,12 @@ def test_prepfiles_raw():
     e_data = pcdcp.data["E"]
     z_data = pcdcp.data["Z"]
     f_data = pcdcp.data["F"]
-    times = pcdcp.times
 
     pcdcp.parse(open("etc/prepfiles/CMO202061_prep.raw").read())
     result_h_data = pcdcp.data["H"]
     result_e_data = pcdcp.data["E"]
     result_z_data = pcdcp.data["Z"]
     result_f_data = pcdcp.data["F"]
-    result_times = pcdcp.times
 
     assert_almost_equal(result_h_data[0:-1], h_data, decimal=2)
     assert_almost_equal(result_e_data[0:-1], e_data, decimal=2)
@@ -34,14 +32,12 @@ def test_prepfiles_min():
     e_data = pcdcp.data["E"]
     z_data = pcdcp.data["Z"]
     f_data = pcdcp.data["F"]
-    times = pcdcp.times
 
     pcdcp.parse(open("etc/prepfiles/CMO202061_prep.min").read())
     result_h_data = pcdcp.data["H"]
     result_e_data = pcdcp.data["E"]
     result_z_data = pcdcp.data["Z"]
     result_f_data = pcdcp.data["F"]
-    result_times = pcdcp.times
 
     assert_almost_equal(result_h_data[0:-1], h_data, decimal=2)
     assert_almost_equal(result_e_data[0:-1], e_data, decimal=2)
