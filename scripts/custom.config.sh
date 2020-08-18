8b0e185ddb28fbb2642f91d4982b6f4787f69c28
Jeremy M Fee <jmfee@usgs.gov>
2020-08-18 15:58:17 +0000

    Add default BASE_HREF.

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -1,10 +1,10 @@
 
 export SITE_URL="${SITE_URL_PREFIX}geomag${SITE_URL_SUFFIX}";
+export BASE_HREF=${BASE_HREF:-ws};
 export SERVICE_MAP=(
   "/${BASE_HREF}":'web'
 );
 # Algorithms Environment Variables
-export DATA_HOST=${DATA_HOST:-cwbpub.cr.usgs.gov}
-export DATA_PORT=${DATA_PORT:-2060}
-export DATA_TYPE=${DATA_TYPE:-edge}
-
+export DATA_HOST=${DATA_HOST:-cwbpub.cr.usgs.gov};
+export DATA_PORT=${DATA_PORT:-2060};
+export DATA_TYPE=${DATA_TYPE:-edge};
