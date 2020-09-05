9759c900b3e69393749aede00063408834a71245
Jeremy Fee <jmfee@usgs.gov>
2020-09-04 18:37:14 -0600

    Update check_code to run script, to allow local gitlab-runner exec

---
+++ scripts/ci_check_code.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+$PYTHON_VERSION=${PYTHON_VERSION:3.8}
+
+
+if [ -f "/etc/profile.d/conda.sh" ]; then
+  # Add conda to path
+  source /etc/profile.d/conda.sh
+fi
+
+# Install Project Dependencies
+conda config --add channels conda-forge
+conda install python=${PYTHON_VERSION} obspy pycurl
+pip install pipenv
+pipenv --site-packages install --dev --pre --skip-lock
+
+# Run Code Checks
+pipenv run black --check .
+pipenv run pytest --cov-report xml:cov.xml --cov=geomagio
