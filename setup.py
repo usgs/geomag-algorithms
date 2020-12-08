70e77335a4a3dfa26c175e8295bcbed93615464a
pcain-usgs <pcain@usgs.gov>
2020-12-08 13:44:50 -0700

    Rename console script

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     entry_points={
         "console_scripts": [
             "magproc-prepfiles=geomagio.processing.magproc:main",
-            "update-legacy=geomagio.processing.obsrio:main",
+            "filter-realtime=geomagio.processing.obsrio:main",
         ],
     },
 )
